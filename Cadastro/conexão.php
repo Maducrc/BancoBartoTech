c58edd099bc6513a6af767e5379921702f8eb1a9
Maria Eduarda Chella <164084351+Maducrc@users.noreply.github.com>
2025-10-03 21:17:43 -0300

    Update conexão.php

--- Cadastro/conexão.php
+++ Cadastro/conexão.php
@@ -2,12 +2,13 @@
 $servidor = 'localhost';
 $bd = 'incentivams';
 $user = 'root';
-$senha = '';
+$senha = '24036M@aria';
 
 try {
-    $verify = new PDO('mysql:host=' .$servidor. ';bdname=' .$bd , $user, $senha);
+    $verify = new PDO('mysql:host=' .$servidor. ';dbname=' . $bd, $user, $senha);
+    $verify->exec("SET time_zone = '-03:00'"); //Define a data e horário corretos
 }
 catch (PDOException $erro) {
     echo 'Houve um erro na conexão: ' .$erro->getMessage();
 }
-?>
+
